2bf2eab84cd7bdb374dd14de3105c4822ceb922b
Ben Forsberg <benj.forsberg@gmail.com>
2022-01-29 06:41:27 +0000

    updated home page, from my ipad via gitpod!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
         <title>CertManager</title>
       </Helmet>
       <h1 className="homepage">Welcome to CertManager!</h1>
+      <p1>I'm slowly learning more React and hope to finish this soon!</p1>
     </div>
   );
 };
